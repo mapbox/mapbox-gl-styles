d8d8d89594923deabf1e53dca172d0074833cb2a
Eden Halperin <eden@mapbox.com>
2014-06-16 12:35:03 -0700

    add bright, outdoors, and pencil

---
+++ index.js
@@ -0,0 +1,4 @@
+module.exports = {};
+module.exports.outdoors_v2 = require('./styles/outdoors-v2.json');
+module.exports.bright_v2 = require('./styles/bright-v2.json');
+module.exports.pencil_v2 = require('./styles/pencil-v2.json');
