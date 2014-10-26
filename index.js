43d19ea7868ba74806bbb44f3ad49fbcefabfb24
John Firebaugh <john.firebaugh@gmail.com>
2014-10-25 23:50:13 -0700

    Update exports

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-module.exports = {};
-module.exports.outdoors_v2 = require('./styles/outdoors-v2.json');
-module.exports.bright_v2 = require('./styles/bright-v2.json');
-module.exports.pencil_v2 = require('./styles/pencil-v2.json');
+[
+    'basic',
+    'bright',
+    'empty',
+    'outdoors-raster',
+    'outdoors',
+    'pencil',
+    'satellite'
+].forEach(function(style) { exports[style] = require('./styles/' + style + '-v5.json'); });
