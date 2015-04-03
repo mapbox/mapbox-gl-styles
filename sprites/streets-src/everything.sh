d29c20d65696249dab1686a0eccdeee71a08ffbb
samanpwbb <samanpwbb@gmail.com>
2015-04-03 16:44:02 -0400

    handle sdf name

--- sprites/streets-src/everything.sh
+++ sprites/streets-src/everything.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 present=$(pwd)
 
-./maki_dots.sh
-./render.sh
+# ./maki_dots.sh
+# ./render.sh
 
 cd ..
 
 gl-style-spritify mapbox-streets streets-src/renders/*
+
+# rename circle
+sed -i '' 's/circle.sdf/circle/g' ${present}/../mapbox-streets.json
+sed -i '' 's/circle.sdf/circle/g' ${present}/../mapbox-streets@2x.json
 
 cd $present
