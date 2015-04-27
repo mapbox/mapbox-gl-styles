fc57428e76d78d28119b77df2291db0254744ef2
Andreas Viglakis <Andreas@Mapbox.com>
2015-04-27 16:11:24 -0700

    light sprites update

--- sprites/light-src/everything.sh
+++ sprites/light-src/everything.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 present=$(pwd)
 
-# ./maki_dots.sh
-# ./render.sh
+./render.sh
 
 cd ..
 
-gl-style-spritify mapbox-streets streets-src/renders/*
-
-# rename circle
-sed -i '' 's/circle.sdf/circle/g' ${present}/../mapbox-streets.json
-sed -i '' 's/circle.sdf/circle/g' ${present}/../mapbox-streets@2x.json
+gl-style-spritify light light-src/renders/
 
 cd $present
