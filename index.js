93a23d762332a4c75e3dae1c547c4d810101e978
samanpwbb <samanpwbb@gmail.com>
2015-05-11 11:58:36 -0400

    only default styles

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 [
-    'basic',
-    'bright',
     'dark',
     'emerald',
-    'empty',
     'light',
     'mapbox-streets',
-    'outdoors',
-    'pencil',
-    'satellite'
 ].forEach(function(style) { exports[style] = require('./styles/' + style + '-v7.json'); });
