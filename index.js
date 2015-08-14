2882a50c91ec11c4327b28a360e8accbae39aa37
Minh Nguyễn <mxn@1ec5.org>
2015-08-14 11:38:33 -0700

    Updated demo page for v8 styles

--- index.js
+++ index.js
@@ -5,8 +5,5 @@
     'emerald',
     'empty',
     'light',
-    'mapbox-streets',
-    'outdoors',
-    'pencil',
-    'satellite'
-].forEach(function(style) { exports[style] = require('./styles/' + style + '-v7.json'); });
+    'streets'
+].forEach(function(style) { exports[style] = require('./styles/' + style + '-v8.json'); });
