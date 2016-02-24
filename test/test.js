4969c2764e6df7d3dca820d8b410d8233893c62e
amyleewalton <amylee@mapbox.com>
2016-02-24 14:52:32 -0500

    new tests for v8 spritesheets and fontstacks

--- test/test.js
+++ test/test.js
@@ -1,13 +1,16 @@
 var test = require('tape');
 var mapboxGL = require('../index');
-var styles = [
+var stylesv7 = [
     'mapbox.dark-v7',
     'mapbox.light-v7',
     'mapbox.emerald-v7',
     'mapbox.streets-v7',
     'mapbox.bright-v7',
     'mapbox.empty-v7',
-    'mapbox.basic-v7',
+    'mapbox.basic-v7'
+];
+
+var stylesv8 = [
     'streets-v8',
     'light-v8',
     'dark-v8',
@@ -29,6 +32,7 @@ var spriteStyles = [
     'emerald-v8'
 ];
 
+// check that all v7 styles exist
 test('.styles', function(t) {
     t.test('should return all styles', function(t){
         Object.keys(mapboxGL.styles).forEach(function(style){
@@ -36,7 +40,7 @@ test('.styles', function(t) {
             t.ok(mapboxGL.styles[style].name, 'Check for version');
             t.ok(mapboxGL.styles[style].layers, 'Check for layers');
         });
-        t.equal(styles.length, Object.keys(mapboxGL.styles).length, 'Check that all styles were hit');
+        t.equal(stylesv7.length+stylesv8.length, Object.keys(mapboxGL.styles).length, 'Check that all styles were hit');
         t.end();
     });
     t.test('should not return a style', function(t){
@@ -47,6 +51,7 @@ test('.styles', function(t) {
     t.end();
 });
 
+// check that all sprites exist
 test('.sprites', function(t) {
     t.test('should return all styles', function(t){
         Object.keys(mapboxGL.sprites).forEach(function(sprite){
@@ -59,6 +64,40 @@ test('.sprites', function(t) {
     });
     t.test('should not return a sprite', function(t){
         var fake = mapboxGL.sprites['foobar'];
+        t.equal(fake, undefined);
+        t.end();
+    });
+    t.end();
+});
+
+
+// check that sprites are properly named
+test('.sprite', function(t) {
+    t.test('should return properly referenced spritesheets', function(t){
+        Object.keys(mapboxGL.styles).forEach(function(stylesv8){
+            t.equal(mapboxGL.styles[stylesv8].sprite, 'mapbox://sprites/mapbox/'+stylesv8, 'References mapbox spritesheet');
+        });
+        t.end();
+    });
+    t.test('should not return a personal spritesheet', function(t){
+        var fake = mapboxGL.styles.sprite;
+        t.equal(fake, undefined);
+        t.end();
+    });
+    t.end();
+});
+
+
+// check that fonts are properly named
+test('.glyphs', function(t) {
+    t.test('should return properly referenced spritesheets', function(t){
+        Object.keys(mapboxGL.styles).forEach(function(stylesv8){
+            t.equal(mapboxGL.styles[stylesv8].glyphs, 'mapbox://fontstack/{fontstack}/{range}.pbf', 'References mapbox glyphs');
+        });
+        t.end();
+    });
+    t.test('should not return a personal fontstack', function(t){
+        var fake = mapboxGL.styles.gylphs;
         t.equal(fake, undefined);
         t.end();
     });
