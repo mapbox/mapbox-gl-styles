04f4b75e345fb8d7125c2be91f634e1ca78d46e2
amyleewalton <amylee@mapbox.com>
2016-02-24 15:52:43 -0500

    clean up code

--- test/test.js
+++ test/test.js
@@ -1,37 +1,5 @@
 var test = require('tape');
 var mapboxGL = require('../index');
-var stylesv7 = [
-    'mapbox.dark-v7',
-    'mapbox.light-v7',
-    'mapbox.emerald-v7',
-    'mapbox.streets-v7',
-    'mapbox.bright-v7',
-    'mapbox.empty-v7',
-    'mapbox.basic-v7'
-];
-
-var stylesv8 = [
-    'streets-v8',
-    'light-v8',
-    'dark-v8',
-    'bright-v8',
-    'basic-v8',
-    'empty-v8',
-    'satellite-v8',
-    'satellite-hybrid-v8',
-    'emerald-v8'
-];
-
-var spriteStyles = [
-    'streets-v8',
-    'light-v8',
-    'dark-v8',
-    'bright-v8',
-    'basic-v8',
-    'satellite-hybrid-v8',
-    'emerald-v8'
-];
-
 
 // check that all v7 styles exist
 test('.styles v7 and v8', function(t) {
@@ -41,20 +9,14 @@ test('.styles v7 and v8', function(t) {
             t.ok(mapboxGL.styles[style].name, 'Check for version');
             t.ok(mapboxGL.styles[style].layers, 'Check for layers');
         });
-        t.equal(stylesv7.length + stylesv8.length, Object.keys(mapboxGL.styles).length, 'Check that all styles were hit');
-        t.end();
-    });
-    t.test('should not return a style', function(t){
-        var fake = mapboxGL.styles['foobar'];
-        t.equal(fake, undefined);
         t.end();
     });
     t.end();
 });
 
 // check that all sprites exist
-test('.sprites v7 and v8', function(t) {
-    t.test('should return all styles', function(t){
+test('.sprites v8', function(t) {
+    t.test('should return all v8 sprites', function(t){
         Object.keys(mapboxGL.sprites).forEach(function(sprite){
             t.ok(sprite.indexOf('v8') > -1);
             t.ok(mapboxGL.sprites[sprite].length > 0);
@@ -63,29 +25,19 @@ test('.sprites v7 and v8', function(t) {
         });
         t.end();
     });
-    t.test('should not return a sprite', function(t){
-        var fake = mapboxGL.sprites['foobar'];
-        t.equal(fake, undefined);
-        t.end();
-    });
     t.end();
 });
 
 // check that sprites are properly named
 test('.spritesheet v8', function(t) {
-    t.test('should return properly referenced spritesheets', function(t){
+    t.test('should return properly referenced spritesheets in v8', function(t){
         Object.keys(mapboxGL.styles).forEach(function(stylesv8){
           var version = mapboxGL.styles[stylesv8].version;
           var name = mapboxGL.styles[stylesv8].name;
-          if (version==8 && name!='Empty') {
+          if (version === 8 && name !== 'Empty') {
             t.equal(mapboxGL.styles[stylesv8].sprite, 'mapbox://sprites/mapbox/' + stylesv8, 'References mapbox spritesheet');
           }
         });
-        t.end();
-    });
-    t.test('should not return a personal spritesheet', function(t){
-        var fake = mapboxGL.styles.name;
-        t.equal(fake, undefined);
         t.end();
     });
     t.end();
@@ -93,19 +45,14 @@ test('.spritesheet v8', function(t) {
 
 // check that fonts are properly named
 test('.glyphs v8', function(t) {
-    t.test('should return properly referenced spritesheets', function(t){
+    t.test('should return properly referenced fontstacks in v8', function(t){
         Object.keys(mapboxGL.styles).forEach(function(stylesv8){
           var version = mapboxGL.styles[stylesv8].version;
-          if (version==8) {
+          if (version === 8) {
             var name = mapboxGL.styles[stylesv8].name;
             t.equal(mapboxGL.styles[stylesv8].glyphs, 'mapbox://fonts/mapbox/{fontstack}/{range}.pbf', 'References mapbox glyphs for ' + name);
           }
         });
-        t.end();
-    });
-    t.test('should not return a personal fontstack', function(t){
-        var fake = mapboxGL.styles.gylphs;
-        t.equal(fake, undefined);
         t.end();
     });
     t.end();
