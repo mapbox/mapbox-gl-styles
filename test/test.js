bde3dbf12476d0e2aa69256d38b551a7afd58965
amyleewalton <amylee@mapbox.com>
2016-03-03 11:37:04 -0800

    test rail for network or maki icons

--- test/test.js
+++ test/test.js
@@ -28,7 +28,6 @@ var maki = [
   'dog-park',
   'drinking-water',
   'embassy',
-  'entrance',
   'fast-food',
   'ferry',
   'fire-station',
@@ -73,7 +72,7 @@ var maki = [
   'volcano',
   'zoo'
 ];
-var rail = [
+var railNetwork = [
   'barcelona-metro',
   'boston-t',
   'chongqing-rail-transit',
@@ -127,79 +126,86 @@ var rail = [
   'vienna-u-bahn',
   'washington-metro'
 ];
-
+var railMaki = [
+  'rail',
+  'rail-metro',
+  'rail-light',
+  'entrance'
+];
 
 // check that all v7 styles exist
 test('.styles v7 and v8', function(t) {
-    t.test('should return all styles', function(t){
-        Object.keys(mapboxGL.styles).forEach(function(style){
-            t.ok(mapboxGL.styles[style].version, 'Check for version');
-            t.ok(mapboxGL.styles[style].name, 'Check for version');
-            t.ok(mapboxGL.styles[style].layers, 'Check for layers');
-        });
-        t.end();
+  t.test('should return all styles', function(t) {
+    Object.keys(mapboxGL.styles).forEach(function(style) {
+      t.ok(mapboxGL.styles[style].version, 'Check for version');
+      t.ok(mapboxGL.styles[style].name, 'Check for version');
+      t.ok(mapboxGL.styles[style].layers, 'Check for layers');
     });
     t.end();
+  });
+  t.end();
 });
 
 // check that all sprites exist
 test('.sprites v8', function(t) {
-    t.test('should return all v8 sprites', function(t){
-        Object.keys(mapboxGL.sprites).forEach(function(sprite){
-            t.ok(sprite.indexOf('v8') > -1);
-            t.ok(mapboxGL.sprites[sprite].length > 0);
-            t.ok(mapboxGL.sprites[sprite][0].id);
-            t.equal(typeof mapboxGL.sprites[sprite][0].svg, 'object');
-        });
-        t.end();
+  t.test('should return all v8 sprites', function(t) {
+    Object.keys(mapboxGL.sprites).forEach(function(sprite) {
+      t.ok(sprite.indexOf('v8') > -1);
+      t.ok(mapboxGL.sprites[sprite].length > 0);
+      t.ok(mapboxGL.sprites[sprite][0].id);
+      t.equal(typeof mapboxGL.sprites[sprite][0].svg, 'object');
     });
     t.end();
+  });
+  t.end();
 });
 
 // check that sprites are properly named
 test('.spritesheet v8', function(t) {
-    t.test('should return properly referenced spritesheets in v8', function(t){
-        Object.keys(mapboxGL.styles).forEach(function(stylesv8){
-          var version = mapboxGL.styles[stylesv8].version;
-          var name = mapboxGL.styles[stylesv8].name;
-          if (version === 8 && name !== 'Empty') {
-            t.equal(mapboxGL.styles[stylesv8].sprite, 'mapbox://sprites/mapbox/' + stylesv8, 'References mapbox spritesheet');
-          }
-        });
-        t.end();
+  t.test('should return properly referenced spritesheets in v8', function(t) {
+    Object.keys(mapboxGL.styles).forEach(function(stylesv8) {
+      var version = mapboxGL.styles[stylesv8].version;
+      var name = mapboxGL.styles[stylesv8].name;
+      if (version === 8 && name !== 'Empty') {
+        t.equal(mapboxGL.styles[stylesv8].sprite, 'mapbox://sprites/mapbox/' + stylesv8, 'References mapbox spritesheet');
+      }
     });
     t.end();
+  });
+  t.end();
 });
 
 // check that fonts are properly named
 test('.glyphs v8', function(t) {
-    t.test('should return properly referenced fontstacks in v8', function(t){
-        Object.keys(mapboxGL.styles).forEach(function(stylesv8){
-          var version = mapboxGL.styles[stylesv8].version;
-          if (version === 8) {
-            var name = mapboxGL.styles[stylesv8].name;
-            t.equal(mapboxGL.styles[stylesv8].glyphs, 'mapbox://fonts/mapbox/{fontstack}/{range}.pbf', 'References mapbox glyphs for ' + name);
-          }
-        });
-        t.end();
+  t.test('should return properly referenced fontstacks in v8', function(t) {
+    Object.keys(mapboxGL.styles).forEach(function(stylesv8) {
+      var version = mapboxGL.styles[stylesv8].version;
+      if (version === 8) {
+        var name = mapboxGL.styles[stylesv8].name;
+        t.equal(mapboxGL.styles[stylesv8].glyphs, 'mapbox://fonts/mapbox/{fontstack}/{range}.pbf', 'References mapbox glyphs for ' + name);
+      }
     });
     t.end();
+  });
+  t.end();
 });
+
 
 // checks all maki icons against list of expected
 test('.maki v8', function(t) {
-  mapboxGL.spriteStyles.forEach(function(style) {
-    // checks string to see if it does contain -v8 and is not in empty-v8
-    if (style.indexOf('-v8') === -1 && style !== 'empty-v8') return;
-    fs.readdir('./sprites/' + style + '/_svg', function(err, files) {
-      if (err) t.fail(err);
-        // console.log(files);
-        maki.forEach(function(name){
+  t.test('checks all maki icons against list of expected maki icons', function(t) {
+    mapboxGL.spriteStyles.forEach(function(style) {
+      if (style.indexOf('-v8') === -1 && style !== 'empty-v8') return;
+      fs.readdir('./sprites/' + style + '/_svg', function(err, files) {
+        if (err) t.fail(err);
+        maki.forEach(function(name) {
           // boolean to see if the value is not false on an array
           t.ok(files.indexOf(name + '-11.svg') !== -1, name + '-11.svg' + ' in ' +style);
           t.ok(files.indexOf(name + '-15.svg') !== -1, name + '-15.svg' + ' in ' +style);
         });
+      });
     });
+    t.end();
   });
   t.end();
 });
@@ -207,26 +213,37 @@ test('.maki v8', function(t) {
 
 // checks all rail icons against list of expected
 test('.rail v8', function(t) {
-  mapboxGL.spriteStyles.forEach(function(style, i) {
-    // pass variable for arrays
-    var totalLayers = mapboxGL.styles[style].layers;
-    var totalSourceLayers = 0;
-    for(i=0; i < totalLayers.length; i++) {
-      var sourceLayer = mapboxGL.styles[style].layers[i]['source-layer'];
-      if(sourceLayer === 'rail_station_label') {
-        totalSourceLayers = totalSourceLayers + 1;
-        if(mapboxGL.styles[style].layers[i].layout['icon-image'] == '{network}') {
-          console.log(style + ' has: ' + totalSourceLayers + ' rail and has network icon');
-          fs.readdir('./sprites/' + style + '/_svg', function(err, files) {
-          if (err) t.fail(err);
-            rail.forEach(function(name){
-              // boolean to see if the value is not false on an array
-              t.ok(files.indexOf(name + '.svg') !== -1, name + '.svg' + ' in ' + style);
+  t.test('checks all rail icons for against either network or maki icon list', function(t) {
+    mapboxGL.spriteStyles.forEach(function(style, i) {
+      // pass variable for arrays
+      var totalLayers = mapboxGL.styles[style].layers;
+      var totalSourceLayers = 0;
+      for(i=0; i < totalLayers.length; i++) {
+        var sourceLayer = mapboxGL.styles[style].layers[i]['source-layer'];
+        if(sourceLayer === 'rail_station_label') {
+          totalSourceLayers = totalSourceLayers + 1;
+          // network if condition
+          if(mapboxGL.styles[style].layers[i].layout['icon-image'] == '{network}') {
+            fs.readdir('./sprites/' + style + '/_svg', function(err, files) {
+              if (err) t.fail(err);
+              railNetwork.forEach(function(name) {
+                t.ok(files.indexOf(name + '.svg') !== -1, name + '.svg' + ' in ' + style);
+              });
             });
-          });
-        }
-      }
-    }
+          } // end network if
+          // maki if condition
+          else if(mapboxGL.styles[style].layers[i].layout['icon-image'] == '{maki}') {
+            fs.readdir('./sprites/' + style + '/_svg', function(err, files) {
+              if (err) t.fail(err);
+              railMaki.forEach(function(name) {
+                t.ok(files.indexOf(name + '.svg') !== -1, name + '.svg' + ' in ' + style);
+              });
+            });
+          } // end maki if
+        } // end sourceLayer if
+      } // end for loop
+    });
+    t.end();
   });
   t.end();
 });
