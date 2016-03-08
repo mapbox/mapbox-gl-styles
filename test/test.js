34e53db51afa7e8cc13ba471cb780127ff0cbe53
amyleewalton <amylee@mapbox.com>
2016-03-07 20:49:29 -0500

    remove broken test for rail maki

--- test/test.js
+++ test/test.js
@@ -130,12 +130,6 @@ var railNetwork = [
   'vienna-u-bahn',
   'washington-metro'
 ];
-var railMaki = [
-  'rail',
-  'rail-metro',
-  'rail-light',
-  'entrance'
-];
 var shields = [
 'at-expressway-2.svg',
 'at-expressway-3.svg',
@@ -369,42 +363,6 @@ test('.rail v8 (network) - checks all network rail icons against list of expecte
     });
   });
 });
-
-// checks all `maki` rail icons against list of expected
-var styleMaki = [];
-var styleValue = [];
-mapboxGL.spriteStyles.forEach(function(style, i) {
-  var totalLayers = mapboxGL.styles[style].layers;
-  for(var i=0; i < totalLayers.length; i++) {
-    var sourceLayer = mapboxGL.styles[style].layers[i]['source-layer'];
-    if(sourceLayer === 'rail_station_label' && mapboxGL.styles[style].layers[i].layout['icon-image'] !== '{network}') {
-      var iconImage =  mapboxGL.styles[style].layers[i].layout['icon-image'];
-      styleMaki.push(style);
-      styleValue.push(mapboxGL.styles[style].layers[i].layout['icon-image']);
-    }
-  }
-});
-for(var i=0; i < styleMaki.length - 1; i++) {
-  styleMaki.forEach(function(style, i) {
-    test('.rail v8 (maki) - checks all maki rail icons against list of expected', function(t) {
-      fs.readdir('./sprites/' + style + '/_svg', function(err, files) {
-        if (err) t.fail(err);
-        railMaki.forEach(function(name) {
-          if(styleValue[i] === '{maki}-11') {
-            t.ok(files.indexOf(name + '-11.svg') !== -1, name + '-11.svg' + ' in ' + style);
-          }
-          if(styleValue[i] === '{maki}-15') {
-            t.ok(files.indexOf(name + '-15.svg') !== -1, name + '-15.svg' + ' in ' + style);
-          }
-          if(styleValue[i] === '{maki}') {
-            t.ok(files.indexOf(name + '.svg') !== -1, name + '.svg' + ' in ' + style);
-          }
-        });
-        t.end();
-      });
-    });
-  });
-}
 
 // checks all highway shields against list of expected
 test('.shields v8 - checks all highway shields against list of expected', function(t) {
