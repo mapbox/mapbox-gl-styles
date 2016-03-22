ec779105e099cef6ed162afdbbca2623f6dfde8f
amyleewalton <amylee@mapbox.com>
2016-03-22 15:47:56 -0400

    compare filters

--- test/town-test.js
+++ test/town-test.js
@@ -10,16 +10,35 @@ test('check that towns are not styled without cities', function(t) {
     var hasCity;
     style.layers.forEach(function(layer) {
       if(layer['source-layer'] === 'place_label') {
-        // find where this filter is matched within all the styles
-        hasTown = _.findWhere(model.layers, {id: 'place town'});
-        // console.log(style.name + ' ' + hasTown.filter);
-        hasCity = _.findWhere(model.layers, {id: 'place city'});
-        // console.log(style.name + ' ' + hasCity.filter);
+        // // find where this filter is matched within all the styles
+        // hasTown = _.findWhere(model.layers, {id: 'place town'});
+        // the layers in the style
+        if(layer.filter.indexOf('all') && layer.filter.indexOf('==') && layer.filter.indexOf('type') && layer.filter.indexOf('town')) {
+          console.log(layer);
+        }
       }
     });
-    if(hasTown !== undefined) {
+    if(hasTown !== undefined && hasTown.filter) {
       t.assert(hasCity, styleName + ' - this has town and has city');
     }
   });
   t.end();
 });
+
+
+// does `Object.values()` work as well as `Object.keys()` it's experimental
+// finding a layer that contains `type` could still be `!==`, need to find 3 words not one
+
+/* when an object contains an object value what is the process for accessing it? like not `model.layers.filter` for:
+   {
+      "filter":[
+        "==",
+        "type",
+        "town"
+      ],
+      "type":"symbol",
+
+      "id":"place town",
+      "source-layer":"place_label"
+    }
+*/
