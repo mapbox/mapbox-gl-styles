2117a45ee9c7ea3da6cde65c5d940f00d78f4455
amyleewalton <amylee@mapbox.com>
2016-03-29 11:26:07 -0400

    checking for bridge and tunnel

--- test/test_check-has-layer.js
+++ test/test_check-has-layer.js
@@ -6,22 +6,29 @@ var mapboxGL = require('../index');
 
 mapboxGL.checkStyles.forEach(function(styles) {
   var style = require('../styles/' + styles + '.json');
-  var found = false;
+  var foundBridge = false;
+  var foundTunnel = false;
+  var foundFord = false;
   test('### Check for mandatory layer styling in : ' + style.name, function(assert) {
     // loop thru all the layers
     style.layers.forEach(function(testLayer) {
       var flatArray = _.flatten(testLayer.filter);
-      var has = _.contains(flatArray, 'tunnel');
-      var has1 = _.contains(flatArray,  '==');
-      var has2 = _.contains(flatArray,  'structure');
-      if(has & has1 && has2) {
-        if(flatArray[0] === 'all' && flatArray[1] === '==' && flatArray[2] === 'structure') {
-          console.log(testLayer.id);
-          found = true;
+      // pre-check for structure and ==
+      var hasEquals = _.contains(flatArray,  '==');
+      var hasStructure = _.contains(flatArray,  'structure');
+      if(hasEquals & hasStructure) { // if we have structure and equals, what is before structure?
+        var inList = _.indexOf(flatArray, 'structure');
+        if(flatArray[inList-1] !== '!in' && flatArray[inList-1] === '==' && flatArray[inList+1] === 'bridge' || flatArray[inList+2] === 'bridge') {
+          // if it's not '!in' is == and bridge is either next or after next, it must be bridge
+          foundBridge = true;
+        } if(flatArray[inList-1] !== '!in' && flatArray[inList-1] == '==' && flatArray[inList+1] == 'tunnel' || flatArray[inList+2] == 'tunnel') {
+          // if it's not '!in' is == and bridge is either next or after next, it must be bridge
+          foundTunnel = true;
         }
       }
-    });
-    assert.ok(found,'Looking for tunnel styling...');
+    }); // end layer loop
+    assert.ok(foundBridge,'Looking for bridge styling...');
+    assert.ok(foundTunnel,'Looking for tunnel styling...');
     assert.end();
-  });
-});
+  }); // end test
+}); // end style loop
