d42ff24139127c4f8e1444435094e4478052abf2
Vladimir Agafonkin <agafonkin@gmail.com>
2026-05-28 12:05:31 +0300

    clean up, remove stale stuff

--- generate_icons_table.js
+++ generate_icons_table.js
@@ -1,17 +1,14 @@
-import icons from 'maki/layouts/streets.js';
+import icons from 'maki/layouts/streets.json' with {type: 'json'};
 
-const base = 'https://cdn.rawgit.com/mapbox/mapbox-gl-styles/master/sprites/bright-v9/_svg';
+const base = 'https://raw.githubusercontent.com/mapbox/mapbox-gl-styles/master/sprites/bright-v9/_svg';
 
 console.log('<table>');
 for (const [category, list] of Object.entries(icons)) {
     console.log(`<tr><th colspan="4">${category}</th></tr>`);
     for (const icon of list) {
-        console.log(`\
-<tr>
-    <td><img src="${base}/${icon}-11.svg"></td>
-    <td><code>${icon}-11</code></td>
-    <td><img src="${base}/${icon}-15.svg"></td>
-    <td><code>${icon}-15</code></td>
+        console.log(`<tr>
+    <td><img src="${base}/${icon}-11.svg"></td><td><code>${icon}-11</code></td>
+    <td><img src="${base}/${icon}-15.svg"></td><td><code>${icon}-15</code></td>
 </tr>`);
     }
 }
